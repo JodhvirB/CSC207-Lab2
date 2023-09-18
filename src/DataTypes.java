33d38392088346e3f2550da5d344c67a21c6be5d
jodhvirbassi <jodhvir81@gmail.com>
2023-09-18 12:07:15 -0400

    hi

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
         }
         return s;
+    }
+
+    public static void main(String[] args) {
+        long x = 500_000_500_000L;
+        List<Integer> lst = new ArrayList<>();
+
+        for (int i = 1; i <= 1_000_000; i++) {
+            lst.add(i);
+            x = sum(lst);
+        }
+        System.out.println(x);
     }
 }
